03e56db23b818b7198b2ba3b3f6c8e287adf74a1
noliw <noliw2017@gmail.com>
2022-08-02 11:33:01 -0700

    isloggedin

--- routes/jobs.js
+++ routes/jobs.js
@@ -9,7 +9,7 @@ router.get("/", jobsCtrl.index);
 router.get("/new", isLoggedIn, jobsCtrl.new);
 //  GET /jobs/:id show
 router.get("/:id", jobsCtrl.show);
-router.get("/:id/edit", jobsCtrl.edit);
+router.get("/:id/edit", isLoggedIn, jobsCtrl.edit);
 router.put("/:id", jobsCtrl.update);
 router.post("/", isLoggedIn, jobsCtrl.create);
 router.delete("/:id", isLoggedIn, jobsCtrl.delete);
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var router = express.Router();
 const notesCtrl = require("../controllers/notes");
+const isLoggedIn = require("../config/auth");
 
 router.post("/jobs/:id/notes", notesCtrl.create);
 
-router.delete("/notes/:id", notesCtrl.delete);
+router.delete("/notes/:id", isLoggedIn, notesCtrl.delete);
 
 module.exports = router;
